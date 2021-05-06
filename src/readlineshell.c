170799f3b753dd78aee001d106b22c8324168e3c
Pieters, Bart <b.pieters@fz-juelich.de>
2021-05-06 20:55:52 +0200

    simplified readlineshell, we do not need select() and the code is more portable without it. For porting ssdp to windows I added "winunistd.h", a drop in replacement of unistd.h for windows platforms.

--- src/readlineshell.c
+++ src/readlineshell.c
@@ -1,35 +1,17 @@
 #include <stdlib.h>
 #include <stdio.h>
-#ifdef _WIN32
-void shell()
-{
-	fprintf(stderr,"Sorry, no interactive shell on windows\n");
-}
-#else
 #include <string.h>
-#include <unistd.h>
-#include <locale.h>
-
-/* Used for select(2) */
-#include <sys/types.h>
-#include <sys/select.h>
-
 #include <signal.h>
-
-#include <stdio.h>
-#include <errno.h>
-
-/* Standard readline include files. */
+#ifdef _WIN64
+#include "winunistd.h"
+#else
+#include <unistd.h>
+#endif /*_WIN64*/
+#include <locale.h>
 #include <readline/readline.h>
 #include <readline/history.h>
-
-
-/* local includes */
 #include "parser.h"
 
-
-static void cb_linehandler (char *);
-static void sighandler (int);
 
 
 int running;
@@ -38,10 +20,13 @@ const char *prompt = ">> ";
 
 /* Handle SIGWINCH and window size changes when readline is not active and
    reading a character. */
+/* SIGWINCH is not present on windows, I just ignore this part then ... */
+#ifndef _WIN64
 static void sighandler (int sig)
 {
 	sigwinch_received = 1;
 }
+#endif /*_WIN64*/
 
 
 char ** keyword_completion(const char *text, int start, int end)
@@ -49,8 +34,6 @@ char ** keyword_completion(const char *text, int start, int end)
     rl_attempted_completion_over = 1;
     return rl_completion_matches(text, keyword_generator);
 }
-
-
 
 /* Callback function called for each line when accept-line executed, EOF
    seen, or EOF character read.  This sets a flag and returns; it could
@@ -82,33 +65,18 @@ static void cb_linehandler (char *line)
 
 void shell()
 {
-	fd_set fds;
 	int r;
     rl_attempted_completion_function = keyword_completion;
-
-	/* Set the default locale values according to environment variables. */
 	setlocale (LC_ALL, "");
-
-
-	/* Handle window size changes when readline is not active and reading
-		characters. */
+#ifndef _WIN64
 	signal (SIGWINCH, sighandler);
-
-	  /* Install the line handler. */
+#endif /*_WIN64*/
 	rl_callback_handler_install (prompt, cb_linehandler);
-
-	  /* Enter a simple event loop.  This waits until something is available
-     to read on readline's input stream (defaults to standard input) and
-     calls the builtin character read callback to read it.  It does not
-     have to modify the user's terminal settings. */
 	running = 1;
 	while (running)
     {
-		FD_ZERO (&fds);
-		FD_SET (fileno (rl_instream), &fds);    
-
-		r = select (FD_SETSIZE, &fds, NULL, NULL, NULL);
-		if (r < 0 && errno != EINTR)
+		r = isatty(STDIN_FILENO);
+		if (r != 1)
 		{
 			fprintf(stderr,"Warning: Problems reading input");
 			rl_callback_handler_remove ();
@@ -119,11 +87,7 @@ void shell()
 			rl_resize_terminal ();
 			sigwinch_received = 0;
 		}
-		if (r < 0)
-			continue;     
-
-		if (FD_ISSET (fileno (rl_instream), &fds))
+		if (r == 1)
 			rl_callback_read_char ();
 	}
 }
-#endif /*_WIN32 */
