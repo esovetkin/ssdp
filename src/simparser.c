125b88b5735c71bfa938be030229e9ee9e62b8d3
Pieters, Bart <b.pieters@fz-juelich.de>
2021-06-01 16:28:44 +0200

    added integrating simulation to compute local potentials

--- src/simparser.c
+++ src/simparser.c
@@ -4,6 +4,9 @@
 #include <ctype.h>
 #include <time.h>
 #include <math.h>
+#ifdef OPENMP
+  #include <omp.h>
+#endif
 /* local includes */
 #include "libssdp.h"
 #include "lio.h"
@@ -18,7 +21,7 @@
 BEGIN_DESCRIPTION
 SECTION Simulation
 PARSEFLAG sim_static SimStatic "C=<in-config> t=<in-array> GHI=<in-array> DHI=<in-array> POA=<out-array>"
-DESCRIPTION Computes the POA irradiance for all configured locations.
+DESCRIPTION Computes the POA irradiance time series for all configured locations. 
 ARGUMENT C Simulation config variable
 ARGUMENT t unix time array.
 ARGUMENT GHI global horizontal irradiance as a function of time
@@ -32,9 +35,7 @@ void SimStatic(char *in)
 	char *word;
 	simulation_config *C;
 	array *t, *GH, *DH, out;
-	clock_t tsky0, tpoa0;
-	clock_t tsky=0, tpoa=0;
-	double ttsky, ttpoa;
+	double tsky, tpoa;
 	int pco=0;
 	word=malloc((strlen(in)+1)*sizeof(char));
 	
@@ -102,22 +103,152 @@ void SimStatic(char *in)
 	}	
 	out.N=t->N*C->Nl;	
 	printf("doing static simulation of %d locations at %d instances\n",C->Nl, t->N);
+	
 	for (j=0;j<t->N;j++)
 	{
 		// compute sky at evert time instance
-		tsky0=clock();
+		printf("%d\n",j);
+		fflush(stdout);
+		TIC();
 		ssdp_make_perez_all_weather_sky_coordinate(&(C->S), (time_t) t->D[j], C->lon, C->lat, GH->D[j], DH->D[j]);
-		tsky+=clock()-tsky0;
-		tpoa0=clock();
-		for (i=0;i<C->Nl;i++)
-			out.D[j*C->Nl+i]=ssdp_total_poa(&(C->S), C->o[i], &(C->M), C->L+i);
-		pco=ProgressBar((100*((j+1)*C->Nl))/(t->N*C->Nl), pco, ProgressLen, ProgressTics);
-		tpoa+=clock()-tpoa0;
+		tsky+=TOC();
+		TIC();
+#pragma omp parallel private(i) shared(C)
+		{
+#pragma omp for 
+			for (i=0;i<C->Nl;i++)
+				out.D[j*C->Nl+i]=ssdp_total_poa(&(C->S), C->o[i], &(C->M), C->L+i);
+		}
+		pco=ProgressBar((100*(j+1))/(t->N), pco, ProgressLen, ProgressTics);
+		tpoa+=TOC();
 	}
-	ttsky=(double)tsky/CLOCKS_PER_SEC;
-	ttpoa=(double)tpoa/CLOCKS_PER_SEC;
-	printf("Computed %d skies in %g s (%g s/sky)\n", t->N, ttsky, ttsky/((double)t->N));
-	printf("Computed %d POA Irradiances in %g s (%g s/POA)\n", t->N*C->Nl, ttpoa, ttpoa/((double)(t->N*C->Nl)));
+	printf("Computed %d skies in %g s (%g s/sky)\n", t->N, tsky, tsky/((double)t->N));
+	printf("Computed %d POA Irradiances in %g s (%g s/POA)\n", t->N*C->Nl, tpoa, tpoa/((double)(t->N*C->Nl)));
+	printf("Creating array %s\n",word);
+	if(AddArray(word, out))
+	{
+		free(word); // failed to make array
+		free(out.D);
+	}	
+}
+
+
+/*
+BEGIN_DESCRIPTION
+SECTION Simulation
+PARSEFLAG sim_static_integral SimStaticInt "C=<in-config> t=<in-array> GHI=<in-array> DHI=<in-array> POA=<out-array>"
+DESCRIPTION Computes the integrated POA irradiance for all configured locations.
+ARGUMENT C Simulation config variable
+ARGUMENT t unix time array.
+ARGUMENT GHI global horizontal irradiance as a function of time
+ARGUMENT DHI diffuse horizontal irradiance as a function of time
+OUTPUT POA plane of array irradiance as a function of time and location (with n time values and m locations the array contains n times m values)
+END_DESCRIPTION
+*/
+void SimStaticInt(char *in)
+{
+	int i, j; // loop through space and time
+	char *word;
+	simulation_config *C;
+	array *t, *GH, *DH, out;
+	double tsky, tpoa;
+	int pco=0;
+	double dt;
+	word=malloc((strlen(in)+1)*sizeof(char));
+	
+	if (FetchConfig(in, "C", word, &C))
+	{
+		free(word);
+		return;
+	}		
+	if (!C->sky_init)
+	{		
+		Warning("Simulation config has no sky initialized\n");
+		free(word);
+		return;
+	}	
+	if ((!C->topo_init)&&(!C->grid_init))
+	{	
+		Warning("No topological data available, omitting horizon\n");
+		InitConfigMaskNoH(C);
+		if (ssdp_error_state)
+		{
+			ssdp_print_error_messages();
+			ssdp_reset_errors();
+			free(word);
+			return;
+		}
+	}
+	if (!C->loc_init) 
+	{	
+		Warning("Simulation config has no locations initialized\n");
+		free(word);
+		return;
+	}		
+	if (FetchArray(in, "t", word, &t))
+	{
+		free(word);
+		return;
+	}
+	if (FetchArray(in, "GHI", word, &GH))
+	{
+		free(word);
+		return;
+	}
+	if (FetchArray(in, "DHI", word, &DH))
+	{
+		free(word);
+		return;
+	}	
+	if ((t->N!=GH->N)||(t->N!=DH->N))
+	{
+		Warning("Length of t-, GHI-, and DHI-arrays do not match\n");
+		free(word);
+		return;
+	}
+	// fetch name of output var
+	if (!GetArg(in, "POA", word))
+	{
+		free(word);
+		return;
+	}
+	out.D=calloc(C->Nl, sizeof(double));
+	if (out.D==NULL)
+	{
+		free(word);
+		return;
+	}	
+	out.N=C->Nl;	
+	printf("doing static integral simulation of %d locations at %d instances\n",C->Nl, t->N);
+	if (t->N==1)
+		dt=1.0;
+	for (j=0;j<t->N;j++)
+	{
+		// compute sky at evert time instance
+		TIC();
+		ssdp_make_perez_all_weather_sky_coordinate(&(C->S), (time_t) t->D[j], C->lon, C->lat, GH->D[j], DH->D[j]);
+		tsky+=TOC();
+		TIC();
+		if (t->N>1)
+		{
+			if (j>0)
+				dt=(t->D[j]-t->D[j-1])/2;
+			else
+				dt=0;
+			if (j<t->N-1)
+				dt+=(t->D[j+1]-t->D[j])/2;
+		}
+#pragma omp parallel private(i) shared(C)
+		{
+#pragma omp for 
+			for (i=0;i<C->Nl;i++)
+				out.D[i]+=ssdp_total_poa(&(C->S), C->o[i], &(C->M), C->L+i)*dt;
+		}
+		pco=ProgressBar((100*(j+1))/(t->N), pco, ProgressLen, ProgressTics);
+		tpoa+=TOC();
+	}
+	printf("Computed %d skies in %g s (%g s/sky)\n", t->N, tsky, tsky/((double)t->N));
+	printf("Computed %d POA Irradiances in %g s (%g s/POA)\n", t->N*C->Nl, tpoa, tpoa/((double)(t->N*C->Nl)));
 	printf("Creating array %s\n",word);
 	if(AddArray(word, out))
 	{
