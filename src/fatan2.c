0f3fbfa269bb3857e4b31b5780444a91c8ca5422
Pieters, Bart <b.pieters@fz-juelich.de>
2021-06-07 16:03:34 +0200

    fixed bug in the fast atan routines while at it made several new fast atan approximations which are either as fast with higher precision or have a higher precision at a moderate cost. The choice of approximation is hardcoded with a define in fatan.c

--- src/fatan2.c
+++ src/fatan2.c
@@ -1,25 +1,39 @@
 #include <math.h>
 #include <float.h>
 // fast atan2 approx
-// subatan: approximate atan(x) on the interval x on [0-1]
-float subatan(float a)
+/* my approximation approach
+ * We only need atan on an interval from 0-1 (the subatan function)
+ * I use polynomials of the form a1*x^n + a2*x^n-1 + ... a1 x + a0 
+ * I fix a0=0 and a1=1 as that is probably not bad anyway
+ * Then I fix the number of terms and maximum order and simply try all 
+ * combinations and select the best precision
+*/
+float subatan_ref(float a)
 {
-	// approximation from some forum
 	float s;
-	s=a*a;
-	return ((-0.0464964749 * s + 0.15931422) * s - 0.327622764) * s * a + a;
-}
-float subatan__(float a)
-{
-	// home brew second order approximation
-	return (-2.7874098272920872e-01*a+1.0641391461266569e+00)*a;
-}
-float subatan_(float a)
-{
-	// first order
-	return 7.85398163397448309628e-01*a;
+	// my reference: approx with 3 terms, max order 6 Emax: 0.0001401683 
+	s=a*a;	
+	return ((-0.095375152306*a+0.212415310139)*s -0.331782140628)*s*a+a;
 }
 
+float subatan_4_9(float a)
+{
+	float s2, s3;
+	// 4 terms, max order 9 Emax: 0.0000035196 (Erel: 0.025110 trel: 1.17)
+	s2=a*a;
+	s3=s2*a;
+	return (((0.006304618756*s3 -0.120631272109)*a + 0.234063762657)*s2 -0.334335841774)*s3+a;
+}
+
+float subatan_5_9(float a)
+{
+	float s;
+	// 5 terms, max order 9 Emax: 0.0000003907 (Erel: 0.002787 trel: 1.28)
+	s=a*a;
+	return ((((-0.033340276431*a+0.140924407553)*a -0.191655659596)*s + 0.202922388832)*s -0.333452613666)*s*a+a;
+}
+
+#define subatan subatan_5_9
 double fatan2(double y, double x)
 {
 	float fx, fy, a, r;
@@ -48,22 +62,21 @@ double fatan2(double y, double x)
 }
 double fatan(double x)
 {
-	float a, r;
+	float fx, a, r;
 	if (x>FLT_MAX)
 		return M_PI/2;
 	if (x<-FLT_MAX)
-		return -M_PI/2;
+		return -M_PI/2;		
 		
-	a=(float)fabs(x);
+	fx=(float)fabs(x);
+	if (fx>1.0)
+		a=1.0/x;
+	else
+		a=fx;
 	r=subatan(a);
-	if (!isfinite(r))
-	{
-		if (x>0)
-			return M_PI/2;
-		else
-			return -M_PI/2;
-	}	
+	if (fx>1.0)
+		r=M_PI/2-r;
 	if (x<0)
-		r=-r;
+		r=M_PI-r;
 	return (double)r;
 }
