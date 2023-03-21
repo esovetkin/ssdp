64a20f1077900c77cc3bad2b2576fc5694eb76b8
Jenya Sovetkin <e.sovetkin@gmail.com>
2023-03-21 08:25:28 +0100

    topogdal: routines for sampling GDAL rasters
    this requires GDAL library (-lgdal flags)

---
+++ src/topogdal.c
@@ -0,0 +1,353 @@
+#include <cpl_conv.h> /* for CPLMalloc() */
+#include <math.h>
+
+#include "error.h"
+#include "epsg.h"
+#include "topogdal.h"
+#include "edt.h"
+
+
+static struct poly4 raster_corners(double gt[6], int xs, int ys)
+{
+        struct poly4 br;
+        bbox2poly4(&br, gt[3], gt[0], gt[3]+ys*gt[5], gt[0]+xs*gt[1]);
+        return br;
+}
+
+
+static int set_geotransform(GDALDatasetH *ds, struct geotransform *gt)
+{
+        GDALGetGeoTransform(*ds, gt->d);
+
+        if (0 == GDALInvGeoTransform(gt->d, gt->i))
+                goto einvtransform;
+
+        gt->nx = GDALGetRasterYSize(*ds);
+        gt->ny = GDALGetRasterXSize(*ds);
+        return 0;
+einvtransform:
+        return 1;
+}
+
+
+// ERRORFLAG GDALNCHANNELNOTONE "Provided raster has channels != 1"
+// ERRORFLAG GDALREADFAILED "Cannot read provided raster"
+struct gdaldata* gdaldata_init(const char *fns[], int nfs)
+{
+        struct gdaldata* self;
+        self = malloc(sizeof(*self));
+        if (NULL == self)
+                goto self_emalloc;
+
+        int i;
+        GDALAllRegister();
+
+        if (NULL == (self->ds = malloc(nfs*sizeof(*self->ds))))
+                goto ds_emalloc;
+        if (NULL == (self->pj = malloc(nfs*sizeof(*self->pj))))
+                goto pj_emalloc;
+        if (NULL == (self->br = malloc(nfs*sizeof(*self->br))))
+                goto br_emalloc;
+        if (NULL == (self->gt = malloc(nfs*sizeof(*self->gt))))
+                goto gt_emalloc;
+
+        self->nds = 0;
+        for (i=0; i < nfs; ++i) {
+                if (NULL == (self->ds[i] = GDALOpen(fns[i], GA_ReadOnly))) {
+                        AddErr(GDALREADFAILED);
+                        goto loop_egdal;
+                }
+
+                self->nds = i + 1;
+
+                // raise problem, when #channels != 1. it supposed to
+                // be elevation raster after all...
+                if (1 != GDALGetRasterCount(self->ds[i])) {
+                        AddErr(GDALNCHANNELNOTONE);
+                        goto loop_egdal;
+                }
+
+                if (set_geotransform(&self->ds[i], &self->gt[i]))
+                        goto loop_egdal;
+
+                self->pj[i] = epsg_init("EPSG:4326",GDALGetProjectionRef(self->ds[i]));
+                if (NULL == self->pj[i])
+                        goto loop_egdal;
+
+                self->br[i] = raster_corners(self->gt[i].d, self->gt[i].nx, self->gt[i].ny);
+        }
+
+        return self;
+loop_egdal:
+        for (i=0; i < self->nds; ++i)
+                GDALClose(self->ds[i]);
+        free(self->gt);
+gt_emalloc:
+        free(self->br);
+br_emalloc:
+        free(self->pj);
+pj_emalloc:
+        free(self->ds);
+ds_emalloc:
+        free(self);
+self_emalloc:
+        return NULL;
+}
+
+
+void gdaldata_free(struct gdaldata* self) {
+        int i;
+
+        for (i=0; i < self->nds; ++i)
+                GDALClose(self->ds[i]);
+
+        if (self->gt)
+                free(self->gt);
+        self->gt = NULL;
+
+        if (self->br)
+                free(self->br);
+        self->br = NULL;
+
+        if (self->pj)
+                free(self->pj);
+        self->pj = NULL;
+
+        if (self->ds)
+                free(self->ds);
+        self->ds = NULL;
+
+        self->nds = 0;
+        free(self);
+        self = NULL;
+}
+
+
+static inline double minp(struct poly4 *p, char what)
+{
+        double a,b,c,d;
+        if ('x' == what) {
+                a = p->p[0].x;
+                b = p->p[1].x;
+                c = p->p[2].x;
+                d = p->p[3].x;
+        } else {
+                a = p->p[0].y;
+                b = p->p[1].y;
+                c = p->p[2].y;
+                d = p->p[3].y;
+        }
+
+        a = (a < b) ? a : b;
+        c = (c < d) ? c : d;
+        return (a < c) ? a : c;
+}
+
+
+static inline double maxp(struct poly4 *p, char what)
+{
+        double a,b,c,d;
+        if ('x' == what) {
+                a = p->p[0].x;
+                b = p->p[1].x;
+                c = p->p[2].x;
+                d = p->p[3].x;
+        } else {
+                a = p->p[0].y;
+                b = p->p[1].y;
+                c = p->p[2].y;
+                d = p->p[3].y;
+        }
+
+        a = (a > b) ? a : b;
+        c = (c > d) ? c : d;
+        return (a > c) ? a : c;
+}
+
+
+static void conv_box(int out[4], struct gdaldata *gd, int i,
+                     struct poly4 cb, int xsize, int ysize)
+{
+        convert_point(gd->pj[i], &cb.p[0], 1);
+        convert_point(gd->pj[i], &cb.p[1], 1);
+        convert_point(gd->pj[i], &cb.p[2], 1);
+        convert_point(gd->pj[i], &cb.p[3], 1);
+
+        struct poly4 pb;
+        GDALApplyGeoTransform(gd->gt[i].i, cb.p[0].y, cb.p[0].x, &pb.p[0].y, &pb.p[0].x);
+        GDALApplyGeoTransform(gd->gt[i].i, cb.p[1].y, cb.p[1].x, &pb.p[1].y, &pb.p[1].x);
+        GDALApplyGeoTransform(gd->gt[i].i, cb.p[2].y, cb.p[2].x, &pb.p[2].y, &pb.p[2].x);
+        GDALApplyGeoTransform(gd->gt[i].i, cb.p[3].y, cb.p[3].x, &pb.p[3].y, &pb.p[3].x);
+
+        out[0] = minp(&pb, 'y');
+        out[1] = minp(&pb, 'x');
+        out[2] = maxp(&pb, 'y') - out[0] + 1;
+        out[3] = maxp(&pb, 'x') - out[1] + 1;
+
+        if (out[0] < 0)
+                out[0] = 0;
+        if (out[0] > ysize)
+                out[0] = ysize;
+        if (out[1] < 0)
+                out[1] = 0;
+        if (out[1] > xsize)
+                out[1] = xsize;
+        if (out[2] + out[0] > ysize)
+                out[2] = ysize - out[0];
+        if (out[3] + out[1] > xsize)
+                out[3] = xsize - out[1];
+}
+
+
+// ERRORFLAG GDALRASTEREMALLOC "Cannot allocate memory for the raster"
+// ERRORFLAG GDALRASTEREREAD "Error on GDALRasterIO!"
+struct raster* raster_init(struct gdaldata *gd, int i, struct poly4 *cb)
+{
+        struct raster *self;
+        self = malloc(sizeof(*self));
+        if (NULL == self)
+                goto self_emalloc;
+
+        GDALRasterBandH hband;
+        hband = GDALGetRasterBand(gd->ds[i], 1);
+
+        int rb[4];
+        conv_box(rb, gd, i, *cb,
+                 GDALGetRasterBandXSize(hband),
+                 GDALGetRasterBandYSize(hband));
+
+        self->xoff = (int) rb[0];
+        self->yoff = (int) rb[1];
+        self->xsize = (int) rb[2];
+        self->ysize = (int) rb[3];
+        self->n = self->xsize * self->ysize;
+
+        // special case when raster has no data
+        if (0 == self->n) {
+                self->d = NULL;
+                return self;
+        }
+
+        self->d = (double*)CPLMalloc(self->n*sizeof(*self->d));
+        if (NULL == self->d) {
+                AddErr(GDALRASTEREMALLOC);
+                goto ecplmalloc;
+        }
+
+        if (GDALRasterIO(hband, GF_Read,
+                         self->xoff, self->yoff,
+                         self->xsize, self->ysize,
+                         self->d,
+                         self->xsize, self->ysize,
+                         GDT_Float64, 0, 0 )) {
+                AddErr(GDALRASTEREREAD);
+                goto erasterio;
+        }
+
+        return self;
+erasterio:
+        CPLFree(self->d);
+ecplmalloc:
+        free(self);
+self_emalloc:
+        return NULL;
+}
+
+
+void raster_free(struct raster *self)
+{
+        if (self->d)
+                CPLFree(self->d);
+        self->d = NULL;
+        self->xsize = 0;
+        self->ysize = 0;
+        free(self);
+        self = NULL;
+}
+
+
+static double get_pixel(struct geotransform *gt, struct raster *r, struct point *p)
+{
+        double u, v;
+
+        // case when raster has no data
+        if (0 == r->n)
+                return -9999.0;
+
+        GDALApplyGeoTransform(gt->i, p->y, p->x, &v, &u);
+        u -= r->yoff;
+        v -= r->xoff;
+
+        if (u < 0 || u >= r->ysize || v < 0 || v >= r->xsize)
+                return -9999.0;
+
+        return r->d[(int) u * r->xsize + (int) v];
+}
+
+
+static double lookup_raster(struct gdaldata *gd, struct raster **r, struct point p)
+{
+        int i;
+        double x, val = -9999.0;
+
+        for (i=0; i < gd->nds; ++i) {
+                convert_point(gd->pj[i], &p, 1);
+
+                x = get_pixel(&gd->gt[i], r[i], &p);
+                val = x > val ? x : val;
+
+                convert_point(gd->pj[i], &p, -1);
+        }
+
+        return val;
+}
+
+
+void free_rasters(struct raster **rd, int nrd)
+{
+        int i;
+        for (i=0; i<nrd; ++i)
+                raster_free(rd[i]);
+        free(rd);
+}
+
+
+double* topogrid_from_gdal(struct gdaldata *gd, struct coordinates *locs)
+{
+        double *z;
+        if (NULL == (z = malloc(locs->np*sizeof(*z))))
+                goto z_emalloc;
+
+        struct raster **rd;
+        if (NULL == (rd = malloc(gd->nds*sizeof(*rd))))
+                goto rd_emalloc;
+
+        // read patches of rasters in memory
+        int i, nrd = 0;
+        for (i=0; i<gd->nds; ++i) {
+                rd[i] = raster_init(gd, i, &locs->br);
+                if (NULL == rd[i]) goto eraster_init;
+                nrd = i + 1;
+        }
+
+        for (i=0; i<locs->np; ++i)
+                z[i] = lookup_raster(gd, rd, locs->p[i]);
+
+        free_rasters(rd, nrd);
+
+        // fill the missing values
+        struct edt *dt = edt_init(z, locs->np, locs->nx, locs->ny);
+        if (NULL == dt) goto eedt_init;
+        edt_compute(dt);
+        edt_fill(dt);
+        edt_free(dt);
+
+        return z;
+eedt_init:
+eraster_init:
+        free_rasters(rd, nrd);
+rd_emalloc:
+        free(z);
+z_emalloc:
+        return NULL;
+}
---
+++ src/topogdal.h
@@ -0,0 +1,112 @@
+#ifndef _TOPOGDAL_H
+#define _TOPOGDAL_H
+
+#include <gdal.h>
+
+#include "epsg.h"
+#include "coordinates.h"
+
+
+/**
+ * data needed for converting coordinates to pixels and back
+ *
+ * @d: geotransform (coordinates -> pixel)
+ * @i: inverse geotransform (pixel -> coordinates)
+ * @nx, @ny: raster shape (x latitude)
+ */
+struct geotransform {
+        double d[6], i[6];
+        int nx, ny;
+};
+
+
+/**
+ * data for several gdal rasters
+ *
+ * required for simulations bbox may involve more than one
+ * raster. this structure stored only the necessary data allowing to
+ * sample those rasters. note, the actual raster data is stored only
+ * while sampling and hence is struct `raster` is used for that.
+ *
+ * @ds: an array of raster bands. We work with elevation rasters (only
+ * 1 band), giving rasters with more or less than 1 bands will result
+ * in a panic.
+ *
+ * @pj: coordinate projection for each of the considered rasters. it
+ * converts epsg:4326 to the coordinate system of each raster.
+ *
+ * @br: 4-polygon describing the area covered by raster. in raster's
+ * coordinates.
+ *
+ * @gt: geotransform data
+ *
+ * @nds: number of raster used
+ */
+struct gdaldata {
+        GDALDatasetH *ds;
+        struct epsg **pj;
+        struct poly4 *br;
+        struct geotransform *gt;
+        int nds;
+};
+
+
+/**
+ * init/free gdaldata
+ *
+ * @fns, @nfns: list of paths for filenames
+ *
+ * returns NULL if:
+ *         - any of the raster is not readable
+ *         - malloc errors
+ *         - rasters have number of bands != 1.
+ */
+struct gdaldata* gdaldata_init(const char **fns, int nfns);
+void gdaldata_free(struct gdaldata*);
+
+
+/**
+ * actual raster data is stored
+ *
+ * @d, @n: data and its length
+ *
+ * @xoff, @yoff, @xsize, @ysize: define the subraster that is stored.
+ */
+struct raster {
+        double *d;
+        int xoff, yoff, xsize, ysize, n;
+};
+
+/**
+ * read raster into memory. only the required box part of the raster
+ * is read.
+ *
+ * @gd, @i: gdaldata and the index of the raster.
+ *
+ * @cb: required polygon in epsg:4326
+ *
+ * returns NULL when failed to read raster or allocate memory
+ */
+struct raster* raster_init(struct gdaldata *gd, int i, struct poly4* cb);
+void raster_free(struct raster *self);
+
+/**
+ * sample georasters at the coordinate locations
+ *
+ * the function allocated crdnts->np array and returns its
+ * pointer. The array contains column-wise points from south-west to
+ * north-east.
+ */
+double* topogrid_from_gdal(struct gdaldata*, struct coordinates*);
+
+// TODO: config_topogdal C=<out-config> x1=<f> y1=<f> x2=<f> y2=<f> step=<f> epsg=<d> a0=<file1-str> a1=<file2-str> ... aN=<fileN-str>
+// x1,y1,x2,y2 define bounding box in epsg:4326
+// step defines step in meters
+// epsg should be optional as determine_epsg gives local utm thing.
+// a0 a1 ... aN defines filepaths
+//
+// config_topogdal initialises topogrid by reading provided files at specific locations
+
+// struct topogrid topogrid_from_gdal(struct gdaldata*, struct coordinates*);
+
+#endif
