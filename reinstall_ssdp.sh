c57c141e5c37126375d419832a5ea2cc2dadcd00
Michael <m.gordon@fz-juelich.de>
2023-05-11 18:18:03 +0200

    added include dir for ubuntu

--- reinstall_ssdp.sh
+++ reinstall_ssdp.sh
@@ -1,6 +1,6 @@
 sudo make uninstall
 make clean
 make distclean
- ./configure CFLAGS="-Wextra -Wpedantic -Wall -Og -g" CC=h5cc
+ ./configure CFLAGS="-Wextra -Wpedantic -Wall -Og -g -I /usr/include/hdf5/serial" CC=h5cc
  make
  sudo make install
