esovetkin/ssdp

commit 6a4e0d7a1116bbc00882f815fd9335a847355407

remove: FetchOpt* functions
why? The code looked identical to me for all cases.

src/arrayparser.c
@@ -573,7 +573,7 @@ void WriteArraysToH5(char *in)
 		Warning("Error: Out of malloc memory!");
 		goto error;
 	}
-	if(FetchOptInt(in, "chunksize", word, &chunk_size)) {
+	if(FetchInt(in, "chunksize", word, &chunk_size)) {
 		chunk_size = 1000;
 		Warning("No argument `chunksize` provided! Using default value %d."
 		" If HDF5 IO performance is poor consider increasing the chunk size or the size of the chunk cache\n", chunk_size);
@@ -674,7 +674,7 @@ void WritePng(char *in)
         if (FetchArray(in, "nx", word, &nx)) goto efetch;
         if (FetchArray(in, "ny", word, &ny)) goto efetch;
 
-        if (FetchOptInt(in, "normalise", word, &n))
+        if (FetchInt(in, "normalise", word, &n))
                 n = 1;
 
         if (0==n)

src/configparser.c
@@ -90,7 +90,7 @@ void ConfigCoord (char *in)
 	C->lat=l->D[0];
 	printf("set latitude to %e degrees (%e rad)\n", rad2deg(C->lat), C->lat);
 	
-	if (FetchOptArray(in, "E", word, &l))
+	if (FetchArray(in, "E", word, &l))
 	{
 		C->E=0;	
 	}
@@ -636,7 +636,7 @@ void ConfigTOPOGDAL (char *in)
                 if (read_filelist(word, fns))
                         goto efnlist;
 
-        if (FetchOptArray(in, "epsg", word, &fepsg)
+        if (FetchArray(in, "epsg", word, &fepsg)
             || 1 != fepsg->N || fepsg->D[0] < 0)
                 epsg = determine_utm((x1+x2)/2, (y1+y2)/2);
         else
@@ -1093,11 +1093,11 @@ void PlaceTemplate(char *in)
         if (FetchFloat(in, "lat", word, &lat)) goto epars;
         if (FetchFloat(in, "lon", word, &lon)) goto epars;
 
-        if (FetchOptFloat(in, "azi", word, &azi))
+        if (FetchFloat(in, "azi", word, &azi))
                 azi = 0;
         azi = deg2rad(azi);
 
-        if (FetchOptInt(in, "epsg", word, &epsg))
+        if (FetchInt(in, "epsg", word, &epsg))
                 epsg = determine_utm(lat, lon);
         struct epsg *pc = epsg_init_epsg(epsg, 4326);
         if (NULL == pc) {

src/parser.c
@@ -223,20 +223,6 @@ int FetchArray(const char *in, const char *pat, char *str, array **a)
 	Warning("Input array %s is not specified\n",pat);
 	return 1;
 }
-int FetchOptArray(const char *in, const char *pat, char *str, array **a)
-{
-	if (GetOption(in, pat, str))
-	{
-		if (!LookupArray(str, a))
-		{
-			Warning("Input array %s=%s, %s is not available\n",pat, str, str);
-			return 1;
-		}
-		return 0;
-	}
-	Warning("Input array %s is not specified\n",pat);
-	return 1;
-}
 int FetchFloat(const char *in, const char *pat, char *str, double *a)
 {
 	if (GetArg(in, pat, str))
@@ -247,29 +233,9 @@ int FetchFloat(const char *in, const char *pat, char *str, double *a)
 	Warning("Input float %s is not specified\n",pat);
 	return 1;
 }
-int FetchOptFloat(const char *in, const char *pat, char *str, double *a)
-{
-	if (GetOption(in, pat, str))
-	{
-		(*a)=atof(str);
-		return 0;
-	}
-	Warning("Input float %s is not specified\n",pat);
-	return 1;
-}
 int FetchInt(const char *in, const char *pat, char *str, int *a)
 {
 	if (GetArg(in, pat, str))
-	{
-		(*a)=atoi(str);
-		return 0;
-	}
-	Warning("Input int %s is not specified\n",pat);
-	return 1;
-}
-int FetchOptInt(const char *in, const char *pat, char *str, int *a)
-{
-	if (GetOption(in, pat, str))
 	{
 		(*a)=atoi(str);
 		return 0;

src/parserutil.h
@@ -36,12 +36,9 @@ int GetNumOption(const char *in, const char *opt, int i, char *word);
 
 int FetchConfig(const char *in, const char *pat, char *str, simulation_config **a);
 int FetchArray(const char *in, const char *pat, char *str, array **a);
-int FetchOptArray(const char *in, const char *pat, char *str, array **a);
 
 int FetchFloat(const char *in, const char *pat, char *str, double *a);
-int FetchOptFloat(const char *in, const char *pat, char *str, double *a);
 int FetchInt(const char *in, const char *pat, char *str, int *a);
-int FetchOptInt(const char *in, const char *pat, char *str, int *a);
 
 void InitConfigMask(simulation_config *C);
 void InitConfigMaskNoH(simulation_config *C);

src/simparser.c
@@ -89,14 +89,14 @@ void SimStatic(char *in)
 		free(word);
 		return;
 	}	
-	if (FetchOptArray(in, "p", word, &p))
+	if (FetchArray(in, "p", word, &p))
 	{
 		pp.N=1;
 		pp.D=malloc(sizeof(double));
 		pp.D[0]=1010.0;
 		p=&pp;
 	}	
-	if (FetchOptArray(in, "T", word, &T))
+	if (FetchArray(in, "T", word, &T))
 	{
 		TT.N=1;
 		TT.D=malloc(sizeof(double));
@@ -266,7 +266,7 @@ void SimStaticInt(char *in)
 		return;
 	}
 		
-	if (FetchOptArray(in, "p", word, &p))
+	if (FetchArray(in, "p", word, &p))
 	{
 		pp.N=t->N;
 		pp.D=malloc(t->N*sizeof(double));
@@ -274,7 +274,7 @@ void SimStaticInt(char *in)
 			pp.D[i]=1010.0;
 		p=&pp;
 	}	
-	if (FetchOptArray(in, "T", word, &T))
+	if (FetchArray(in, "T", word, &T))
 	{
 		TT.N=t->N;
 		TT.D=malloc(t->N*sizeof(double));
@@ -434,14 +434,14 @@ void SimRoute(char *in)
 		return;
 	}	
 	
-	if (FetchOptArray(in, "p", word, &p))
+	if (FetchArray(in, "p", word, &p))
 	{
 		pp.N=1;
 		pp.D=malloc(sizeof(double));
 		pp.D[0]=1010.0;
 		p=&pp;
 	}	
-	if (FetchOptArray(in, "T", word, &T))
+	if (FetchArray(in, "T", word, &T))
 	{
 		TT.N=1;
 		TT.D=malloc(sizeof(double));
@@ -606,14 +606,14 @@ void SimStaticUniform(char *in)
 		free(word);
 		return;
 	}
-	if (FetchOptArray(in, "p", word, &p))
+	if (FetchArray(in, "p", word, &p))
 	{
 		pp.N=1;
 		pp.D=malloc(sizeof(double));
 		pp.D[0]=1010.0;
 		p=&pp;
 	}	
-	if (FetchOptArray(in, "T", word, &T))
+	if (FetchArray(in, "T", word, &T))
 	{
 		TT.N=1;
 		TT.D=malloc(sizeof(double));
@@ -755,10 +755,10 @@ void SolarPos(char *in)
 	}
 	lon=deg2rad(lon);
 	lat=deg2rad(lat);
-	if (FetchOptFloat(in, "E", word, &E))
+	if (FetchFloat(in, "E", word, &E))
 		E=0;
 		
-	if (FetchOptArray(in, "p", word, &p))
+	if (FetchArray(in, "p", word, &p))
 	{
 		// make a scalar array
 		pp.N=1;
@@ -766,7 +766,7 @@ void SolarPos(char *in)
 		pp.D[0]=1010.0;
 		p=&pp;
 	}
-	if (FetchOptArray(in, "T", word, &T))
+	if (FetchArray(in, "T", word, &T))
 	{
 		// make a scalar array
 		TT.N=1;
@@ -915,10 +915,10 @@ void SolarTimes(char *in)
 	}
 	lat=deg2rad(lat);
 	
-	if (FetchOptFloat(in, "E", word, &E))
+	if (FetchFloat(in, "E", word, &E))
 		E=0;
 		
-	if (FetchOptArray(in, "p", word, &p))
+	if (FetchArray(in, "p", word, &p))
 	{
 		// make a scalar array
 		pp.N=1;
@@ -926,7 +926,7 @@ void SolarTimes(char *in)
 		pp.D[0]=1010.0;
 		p=&pp;
 	}
-	if (FetchOptArray(in, "T", word, &T))
+	if (FetchArray(in, "T", word, &T))
 	{
 		// make a scalar array
 		TT.N=1;
@@ -1137,14 +1137,14 @@ void ExportSky(char *in)
 		free(word);
 		return;
 	}	
-	if (FetchOptArray(in, "p", word, &p))
+	if (FetchArray(in, "p", word, &p))
 	{
 		pp.N=1;
 		pp.D=malloc(sizeof(double));
 		pp.D[0]=1010.0;
 		p=&pp;
 	}	
-	if (FetchOptArray(in, "T", word, &T))
+	if (FetchArray(in, "T", word, &T))
 	{
 		TT.N=1;
 		TT.D=malloc(sizeof(double));

commit 8736eafebcf7bf393c5f1a5c5f0e0530486486e1

fix: regression from 6a4e0d7a11
I mistakenly thought that FetchOpt{Int,Float} play no role.

src/arrayparser.c
@@ -450,10 +450,10 @@ void WriteH5(char *in)
 
         if (!GetOption(in, "type", word)) snprintf(word, strlen(in), "float64");
         h5io_setdtype(io, word);
-        if (FetchInt(in, "gzip", word, &io->compression)) io->compression = 0;
-        if (FetchInt(in, "chunksize", word, &io->chunkarr)) io->chunkarr = 1;
-        if (FetchInt(in, "cachemb", word, &io->cachemb)) io->cachemb = 64;
-        if (FetchInt(in, "cacheslots", word, &io->cacheslots)) io->cacheslots = 12421;
+        if (FetchOptInt(in, "gzip", word, &io->compression)) io->compression = 0;
+        if (FetchOptInt(in, "chunksize", word, &io->chunkarr)) io->chunkarr = 1;
+        if (FetchOptInt(in, "cachemb", word, &io->cachemb)) io->cachemb = 64;
+        if (FetchOptInt(in, "cacheslots", word, &io->cacheslots)) io->cacheslots = 12421;
 
         TIC();
         if (h5io_write(io, data, arrlen, narr)) goto ewrite;
@@ -568,7 +568,7 @@ void WritePng(char *in)
         if (FetchArray(in, "z", word, &z)) goto efetch;
         if (FetchInt(in, "nx", word, &nx)) goto efetch;
         if (FetchInt(in, "ny", word, &ny)) goto efetch;
-        if (FetchInt(in, "normalise", word, &norm))
+        if (FetchOptInt(in, "normalise", word, &norm))
                 norm = 1;
 
         if (0==norm)
@@ -1081,11 +1081,11 @@ void Perturb(char *in)
 	int i;
 	array *x;
 	word=malloc((strlen(in)+1)*sizeof(char));
-	if (FetchFloat(in, "releps", word, &releps))
+	if (FetchOptFloat(in, "releps", word, &releps))
             releps = 0.0;
 	releps=fabs(releps);
 
-	if (FetchFloat(in, "abseps", word, &abseps))
+	if (FetchOptFloat(in, "abseps", word, &abseps))
             abseps = 0.0;
 	abseps=fabs(abseps);
 

src/configparser.c
@@ -78,7 +78,7 @@ void ConfigCoord (char *in)
 	C->lat=l;
 	printf("set latitude to %e degrees (%e rad)\n", rad2deg(C->lat), C->lat);
 	
-	if (FetchFloat(in, "E", word, &l))
+	if (FetchOptFloat(in, "E", word, &l))
         l = 0;
 
     C->E=l;
@@ -613,7 +613,7 @@ void ConfigTOPOGDAL (char *in)
                 if (read_filelist(word, fns))
                         goto efnlist;
 
-        if (FetchInt(in, "epsg", word, &epsg))
+        if (FetchOptInt(in, "epsg", word, &epsg))
                 epsg = determine_utm((x1+x2)/2, (y1+y2)/2);
 
         printf("Sampling with step=%.3f epsg=%d\n"
@@ -736,7 +736,7 @@ void ConfigLoc (char *in)
 		if (C->grid_init==0)
 			Warning("No topology or topogrid available\n");
 	}
-	if (FetchFloat(in, "albedo", word, &(C->albedo)))
+	if (FetchOptFloat(in, "albedo", word, &(C->albedo)))
 		C->albedo=0.0;
 
 	free(word);
@@ -1060,11 +1060,11 @@ void PlaceTemplate(char *in)
         if (FetchFloat(in, "lat", word, &lat)) goto epars;
         if (FetchFloat(in, "lon", word, &lon)) goto epars;
 
-        if (FetchFloat(in, "azi", word, &azi))
+        if (FetchOptFloat(in, "azi", word, &azi))
                 azi = 0;
         azi = deg2rad(azi);
 
-        if (FetchInt(in, "epsg", word, &epsg))
+        if (FetchOptInt(in, "epsg", word, &epsg))
                 epsg = determine_utm(lat, lon);
         struct epsg *pc = epsg_init_epsg(epsg, 4326);
         if (NULL == pc) {

src/parser.c
@@ -223,10 +223,10 @@ int FetchArray(const char *in, const char *pat, char *str, array **a)
 	Warning("Input array %s is not specified\n",pat);
 	return 1;
 }
-int FetchFloat(const char *in, const char *pat, char *str, double *a)
+int FetchOptFloat(const char *in, const char *pat, char *str, double *a)
 {
         array *x;
-        if (! GetArg(in, pat, str))
+        if (! GetOption(in, pat, str))
                 goto noarg;
 
         if (LookupArray(str, &x))
@@ -240,13 +240,19 @@ array:
         (*a) = x->D[0];
         return 0;
 noarg:
-        Warning("Input float %s is not specified\n", pat);
         return 1;
 }
-int FetchInt(const char *in, const char *pat, char *str, int *a)
+int FetchFloat(const char *in, const char *pat, char *str, double *a)
+{
+        int x = FetchOptFloat(in, pat, str, a);
+        if (x)
+                Warning("Error: input float %s is not specified\n", pat);
+        return x;
+}
+int FetchOptInt(const char *in, const char *pat, char *str, int *a)
 {
         array *x;
-        if (! GetArg(in, pat, str))
+        if (! GetOption(in, pat, str))
                 goto noarg;
 
         if (LookupArray(str, &x))
@@ -256,12 +262,18 @@ int FetchInt(const char *in, const char *pat, char *str, int *a)
         return 0;
 array:
         if (1 != x->N)
-                Warning("non-unit array provided for %s\n", pat);
+                Warning("Warning: non-unit array provided for %s\n", pat);
         (*a) = (int) x->D[0];
         return 0;
 noarg:
-        Warning("Input int %s is not specified\n", pat);
         return 1;
+}
+int FetchInt(const char *in, const char *pat, char *str, int *a)
+{
+        int x = FetchOptInt(in, pat, str, a);
+        if (x)
+                Warning("Error: input integer %s is not specified\n", pat);
+        return x;
 }
 
 /* 

src/parserutil.h
@@ -38,7 +38,9 @@ int FetchConfig(const char *in, const char *pat, char *str, simulation_config **
 int FetchArray(const char *in, const char *pat, char *str, array **a);
 
 int FetchFloat(const char *in, const char *pat, char *str, double *a);
+int FetchOptFloat(const char *in, const char *pat, char *str, double *a);
 int FetchInt(const char *in, const char *pat, char *str, int *a);
+int FetchOptInt(const char *in, const char *pat, char *str, int *a);
 
 void InitConfigMask(simulation_config *C);
 void InitConfigMaskNoH(simulation_config *C);